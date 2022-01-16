d4e7c00fbadd9751411a841bdef7af8c08f749d7
andydevs <akanshul97@gmail.com>
2022-01-16 00:49:02 -0500

    Implemented gravity mechanics

---
+++ Assets/Scripts/Gravitator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Gravitator : MonoBehaviour
+{
+    public const float G = 1000.0f;
+
+    public float mass = 1.0f;
+
+    private Rigidbody2D golfball;
+    private CircleCollider2D surface;
+
+    void Start()
+    {
+        // Find components
+        golfball = GameObject
+            .FindGameObjectWithTag("Player")
+            .GetComponent<Rigidbody2D>();
+        surface = GetComponent<CircleCollider2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // Gravity.... it idk
+        Vector2 relative = transform.position + (Vector3)surface.offset - golfball.transform.position;
+        float gravity = G * mass * golfball.mass / relative.sqrMagnitude;
+        golfball.AddForce(gravity * relative.normalized);
+    }
+}
---
+++ Assets/Scripts/SwingController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = 8*Vector2.right;
+    }
+}
