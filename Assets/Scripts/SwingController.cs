d6df64b003539fd9cd98a9714e72336ab93016c8
andydevs <akanshul97@gmail.com>
2022-01-19 23:24:16 -0500

    Add temporary swing physics controller

--- Assets/Scripts/SwingController.cs
+++ Assets/Scripts/SwingController.cs
@@ -1,15 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SwingController : MonoBehaviour
 {
-    private Rigidbody2D rigidbody2D;
+    public float strokeSpeed = 1.0f;
+
+    private Rigidbody2D rigidbody2D_;
+
+    public bool IsInStroke { get { return rigidbody2D_.simulated; } }
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-        rigidbody2D.velocity = 8*Vector2.right;
+        rigidbody2D_ = GetComponent<Rigidbody2D>();
+        rigidbody2D_.simulated = false;
+    }
+
+    public void OnSwing()
+    {
+        // Get mouse poisition
+        Vector2 screenSpaceMouse = Mouse.current.position.ReadValue();
+        Vector2 worldSpaceMouse = Camera.main.ScreenToWorldPoint(screenSpaceMouse);
+        Debug.Log(worldSpaceMouse);
+
+        // Determine argument
+        Vector2 argument = worldSpaceMouse - (Vector2)transform.position;
+        argument.Normalize();
+
+        // Simulate physics
+        if (!rigidbody2D_.simulated)
+        {
+            rigidbody2D_.velocity = strokeSpeed * argument;
+            rigidbody2D_.simulated = true;
+        }
     }
 }
