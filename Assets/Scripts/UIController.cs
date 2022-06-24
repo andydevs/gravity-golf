d4aec2d6a1e957847d8df688f9667cc8484cdd99
andydevs <akanshul97@gmail.com>
2022-06-23 23:03:06 -0400

    Backburnering project for now

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    // Game UI
+    private GameObject winUI;
+    private GameObject gameUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winUI = transform.Find("Win Screen").gameObject;
+        gameUI = transform.Find("In Game Screen").gameObject;
+
+        // Make sure initial state is set
+        winUI.SetActive(false);
+        gameUI.SetActive(true);
+
+        // Subscribe to player end
+        PlayerController.OnPlayerEnd += OnGameEnd;
+    }
+
+    void OnGameEnd(bool didit, int playerId, int strokes)
+    {
+        winUI.SetActive(true);
+    }
+
+    void OnRestart()
+    {
+        winUI.SetActive(false);
+    }
+
+    void OnQuit()
+    {
+        Application.Quit();
+    }
+}
