1dc4470ec3e940d39ecdc1829f00d6a5a14129ca
andydevs <akanshul97@gmail.com>
2022-11-13 21:29:57 -0500

    Make camera follow ball exactly

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    // Ball end event
     public delegate void BallEnd(bool didit);
     public BallEnd OnBallEnd;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        // Set position of camera
+        Camera.main.transform.position = new Vector3(
+            transform.position.x, 
+            transform.position.y, 
+            -10);
+    }
 
     void OnHole()
     {
