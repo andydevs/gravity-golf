b703cde295fea2c36f7c57e403fea07ce448206d
andydevs <akanshul97@gmail.com>
2022-11-13 21:42:40 -0500

    Camera smooth follows ball

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -8,18 +8,21 @@ public class BallController : MonoBehaviour
     public delegate void BallEnd(bool didit);
     public BallEnd OnBallEnd;
 
-    void Start()
-    {
-        
-    }
+    // Camera follow rate
+    float cameraFollowRate = 10.0f;
 
     void FixedUpdate()
     {
         // Set position of camera
-        Camera.main.transform.position = new Vector3(
-            transform.position.x, 
-            transform.position.y, 
-            -10);
+        Camera.main.transform.position = 
+            Vector3.Lerp(
+                Camera.main.transform.position,
+                new Vector3(
+                    transform.position.x,
+                    transform.position.y,
+                    -10),
+                cameraFollowRate*Time.fixedDeltaTime
+            );
     }
 
     void OnHole()
