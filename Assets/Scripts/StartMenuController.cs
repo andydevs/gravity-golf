64daba6a4e3a93c93eb01aa6e85301aa81e0ddc0
andydevs <akanshul97@gmail.com>
2022-12-31 01:49:28 -0500

    Add Credits UI

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -5,11 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour
 {
+    // Data
     public string startLevel = "Level 1";
+
+    // UIs
+    private GameObject menuUI;
+    private GameObject creditUI;
+
+    void Start()
+    {
+        menuUI = transform.Find("Main Menu").gameObject;
+        creditUI = transform.Find("Credits").gameObject;
+
+        menuUI.SetActive(true);
+        creditUI.SetActive(false);
+    }
 
     void OnStart()
     {
         SceneManager.LoadScene(startLevel);
+    }
+
+    void OnCredits()
+    {
+        menuUI.SetActive(false);
+        creditUI.SetActive(true);
+    }
+
+    void OnMenu()
+    {
+        menuUI.SetActive(true);
+        creditUI.SetActive(false);
     }
 
     void OnQuit()
